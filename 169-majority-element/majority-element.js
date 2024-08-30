cab97a04363f2c4d084eb9556827e93ccf8101d1
Tapan Mehta <mehtatapan44@gmail.com>
2024-08-30 17:10:26 -0400

    Time: 68 ms (24.61%) | Memory: 50.9 MB (86.98%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -4,7 +4,7 @@
  */
 var majorityElement = function(nums) {
     let curr = nums[0];
-    let count = 1;
+    let count =1;
     for(i = 1; i < nums.length; i++){
         nums[i] === curr ? count ++ : count--;
         if(count < 0){
