5a90ca8d90eb98ebbd326741f2d075c61ba2fda6
Tapan Mehta <mehtatapan44@gmail.com>
2024-08-30 17:26:03 -0400

    Time: 1258 ms (10.15%) | Memory: 72.6 MB (65.80%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.js
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -3,19 +3,20 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    nums = nums.sort((a,b) => a - b);
-    nums = Array.from(new Set(nums));
-    let currLength = 1;
-    let maxLength = 0;
+    const numSet = new Set(nums);
+    let longest = 0;
 
-    for(let i = 0; i < nums.length; i++){
-        if(i !== 0 && nums[i] - 1 === nums[ i - 1 ]){
-             currLength++;
-        } else {
-            currLength = 1;
+    for (let n of nums) {
+        if (!numSet.has(n - 1)) {
+            let length = 1;
+
+            while (numSet.has(n + length)) {
+                length++;
+            }
+
+            longest = Math.max(longest, length);
         }
-        maxLength = Math.max(currLength, maxLength);
     }
 
-    return maxLength;
+    return longest;
 };
