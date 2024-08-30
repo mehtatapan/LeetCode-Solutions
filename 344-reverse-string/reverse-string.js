0bdb756ece7a9b5b7db4443418762831cf11fcd2
Tapan Mehta <mehtatapan44@gmail.com>
2024-08-30 17:44:18 -0400

    Time: 69 ms (89.68%) | Memory: 57.1 MB (48.44%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,5 +3,11 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse();
+    let left = 0;
+    const mid = Math.floor(s.length / 2);
+    while(left < mid){
+        [s[left], s[(s.length -1) - left]] = [s[(s.length -1) - left], s[left]];
+        left++;
+    }
+    return s;
 };
