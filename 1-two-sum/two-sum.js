5289d24cc8b46610dff70200d7d94a652ff82800
Tapan Mehta <mehtatapan44@gmail.com>
2024-08-30 16:08:42 -0400

    Time: 35 ms (99.97%) | Memory: 51.8 MB (18.02%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -7,7 +7,7 @@ var twoSum = function(nums, target) {
     var mySet = new Set();
     var ans = [0,0];
     nums.forEach((ele,i)=>{
-        mySet.has(target - ele) ? ans = [nums.indexOf(target - ele), i] : mySet.add(ele);
+        mySet.has(target - ele) ?ans = [nums.indexOf(target - ele), i] : mySet.add(ele);
     });
     return ans;
 };
