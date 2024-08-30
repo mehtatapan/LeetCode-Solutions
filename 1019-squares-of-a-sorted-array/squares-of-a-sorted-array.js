500671e67dab3947f0612dd73f2839fca356899c
Tapan Mehta <mehtatapan44@gmail.com>
2024-08-30 17:35:50 -0400

    Time: 79 ms (75.51%) | Memory: 56.5 MB (44.81%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,7 +3,24 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    nums.forEach((num, i) => nums[i] = num * num);
-    nums.sort((a,b) => a-b);
-    return nums;
+    let n = nums.length;
+    let result = new Array(n);
+    let left = 0;
+    let right = n - 1;
+    let index = n - 1;
+
+    while(left <= right){
+        let leftSq = nums[left] * nums[left];
+        let rightSq = nums[right] * nums[right];
+
+        if(leftSq > rightSq){
+            result[index] = leftSq;
+            left++;
+        } else{
+            result[index] = rightSq;
+            right--;
+        }
+        index--;
+    }
+    return result;
 };
