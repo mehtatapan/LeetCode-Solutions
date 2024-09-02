36da896ec34d9fe283d1a59233950fc7d09c7e1c
Tapan Mehta <mehtatapan44@gmail.com>
2024-09-01 23:28:40 -0400

    Time: 62 ms (93.58%) | Memory: 52.7 MB (60.44%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 const reverse = (x) => {
-    if(x<0) return (-1)* reverse(-x);
+    if(x < 0) return (-1)* reverse(-x);
     const result = parseInt(x.toString().split('').reverse().join(''));
     return result < Math.pow(2, 31) - 1 ? result : 0;
 };
